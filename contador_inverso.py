5473791dda7ddd5a059d027669d0a853245c37d6
Ragnarok912-star <okite1995@gmail.com>
2026-06-13 17:59:26 -0500

    Contador regresivo..

---
+++ contador_inverso.py
@@ -0,0 +1,8 @@
+num = int(input("Introduce un numero entero: "))
+
+while True:
+      num -= 1
+      print (f"Conteo regresivo... {num} ")
+      if num == 0:
+         print ("Tiempo agotado.")
+         break
