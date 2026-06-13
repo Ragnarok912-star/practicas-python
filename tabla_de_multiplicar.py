20d49817f4adf03ddf65bc3a601be073a9bdc4a8
Ragnarok912-star <okite1995@gmail.com>
2026-06-13 12:57:11 -0500

    Tabla de multiplicar

---
+++ tabla_de_multiplicar.py
@@ -0,0 +1,14 @@
+c =1
+i =0
+while True:
+    i += 1
+    resultado = c * i
+    print (f"{c} x {i} = {resultado}")
+    
+    if i == 9:
+       print (end="")
+       c += 1
+       i = 0
+    if c == 9:
+       break
+
