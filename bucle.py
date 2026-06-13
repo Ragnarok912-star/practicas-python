9d3ef51a0dcb94e066f118d0825698739950f597
Ragnarok912-star <okite1995@gmail.com>
2026-06-13 17:41:44 -0500

    Ejercicio sencillo de bucle for

--- bucle.py
+++ bucle.py
@@ -17,22 +17,3 @@ for pais, capi in capitales.items():
 
 for long in range(6):
     print(long)
-
-#tablas de multiplicar 
-
-num = 1 
-num2 = 0
-
-
-while True:
-    num2 += 1
-    r = num * num2 
-    print (f"{num} x {num2} = {r}")
-
-    if num2 == 10:
-       num += 1
-       num2 = 0
-       print ("\n")
-    if num == 30:
-       break
-       
