ead4c9f97a15aeda963d092abbe0e86cc1eed558
Ragnarok912-star <okite1995@gmail.com>
2026-06-14 18:53:59 -0500

    Scanenr de ips

--- scanner_de_red.py
+++ scanner_de_red.py
@@ -3,15 +3,17 @@ import time
 
 def medir(ips):
     equi_online = 0
-
+    
     for ip in ips:
         print(f"Haciendo ping a la ip {ip}...")
         time.sleep(1.5)
-
+        
+        # Al cumplir la condición, lo de abajo lleva 4 espacios extra a la derecha
         if int(ip.split('.')[-1]) % 2 == 0:
-            equi_online += 1
+            equi_online += 1 
 
-    print ("Equipos en linea: ", equi_online)
+    # Fuera del bucle for para que se ejecute solo al final de revisar todas las IPs
+    print("Equipos en linea: ", equi_online)
 
-ips = ["192.168.1.80","192.168.1.5","192.168.1.2"]
+ips = ["192.168.1.80", "192.168.1.5", "192.168.1.2"]
 medir(ips)
